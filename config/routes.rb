e99c48fd4ba87901aae940b62812250c0e3733df
Ben Garretson <bgarret39@gmail.com>
2018-07-11 15:32:20 +0000

    variables just added, stil not interactive

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 get'todo/index', to:'todo#index'
 get'todo/AddTask', to:'todo#AddTask'
-get'todo/show', to:'todo#show'
+get'todo/show/:id', to:'todo#show'
 
 
 end
