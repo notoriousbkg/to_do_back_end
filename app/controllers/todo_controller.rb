b3712d4ab3197bb45d84b14e1f3be4138abb7c1b
Ben Garretson <bgarret39@gmail.com>
2018-07-11 15:01:04 +0000

    variables just added, stil not interactive

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,18 @@
 class TodoController < ApplicationController
     def index
+        @todo_name="Eat food"
+        @todo_time="100000 min"
+        @todo_days="3"
+        @todo_description="eat all the food, leave none"
     end
+    def AddTask
+    end
+    def show
+        @todo_name="Eat food"
+        @todo_time="100000 min"
+        @todo_days="3"
+        @todo_description="eat all the food, leave none"
+    end
+    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 get'todo/index', to:'todo#index'
-
+get'todo/AddTask', to:'todo#AddTask'
+get'todo/show', to:'todo#show'
 
 
 end
